7e1f23515d960b9a6ea42cf278c67348a56206ab
Vincent De Smet <vincent@goodnotesapp.com>
2023-10-24 15:12:32 +0700

    chore: Switch to GH Packages

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,13 +2,16 @@ import { cdk8s } from "projen";
 const project = new cdk8s.ConstructLibraryCdk8s({
   author: "Vincent De Smet",
   authorAddress: "vincent@goodnotesapp.com",
-  cdk8sVersion: "1.4.10",
+  cdk8sVersion: "2.67.2",
   defaultReleaseBranch: "main",
   jsiiVersion: "~5.0.0",
   name: "cdk8s-datadog",
   projenrcTs: true,
   repositoryUrl: "https://github.com/vincent/cdk8s-datadog.git",
   prettier: true,
+  eslint: true,
+  releaseToNpm: true,
+  npmRegistryUrl: "https://npm.pkg.github.com",
   sampleCode: false,
 });
 if (project.prettier?.ignoreFile) {
